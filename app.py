edd9ed2ebaf98d0c723126d25c02ad614bb055c0
ogorovoy <ogorovoy@icloud.com>
2024-03-04 15:59:11 +0100

    updates file , add func delete event

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 
 def load_events():
-    with open('../calendar/data/data.json', 'r') as file:
+    with open('../Flask_calendar/data/data.json', 'r') as file:
         events = json.load(file)
     return events
 
 
 def save_events(events):
-    with open('../calendar/data/data.json', 'w') as file:
+    with open('../Flask_calendar/data/data.json', 'w') as file:
         json.dump(events, file, indent=4)
 
 
@@ -32,6 +32,17 @@ def add_event():
     events = load_events()
     events.append(data)
     save_events(events)
+    return jsonify(success=True)
+
+@app.route('/delete_event', methods=['POST'])
+def delete_event():
+    data = request.json
+    title_to_delete = data.get('title')
+
+    events = load_events()
+    events = [event for event in events if event['title'] != title_to_delete]
+    save_events(events)
+
     return jsonify(success=True)
 
 if __name__ == "__main__":
